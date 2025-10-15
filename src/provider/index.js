aaae5cb42d6498c7440eac1f29cb47927ba47f50
jsoliz064 <jsoliz064@gmail.com>
2025-10-15 00:57:48 -0400

    update version provider

--- src/provider/index.js
+++ src/provider/index.js
@@ -2,5 +2,5 @@ const { createProvider } = require("@builderbot/bot");
 const { BaileysProvider } = require("@builderbot/provider-baileys");
 
 module.exports = createProvider(BaileysProvider, {
-  version: [2, 3000, 1023223821],
+  version: [2, 3000, 1025190524],
 });
