241e119ed01012a1050bfa347a1698f1ed5a163e
jsoliz064 <jsoliz064@gmail.com>
2025-12-11 12:28:03 -0400

    update provder

--- src/provider/index.js
+++ src/provider/index.js
@@ -1,8 +1,8 @@
 import { createProvider } from "@builderbot/bot";
-// import { BaileysProvider } from "@builderbot/provider-baileys";
-import { SherpaProvider } from "@builderbot/provider-sherpa";
+import { BaileysProvider } from "@builderbot/provider-baileys";
+// import { SherpaProvider } from "@builderbot/provider-sherpa";
 
-export default createProvider(SherpaProvider, {
+export default createProvider(BaileysProvider, {
   // usePairingCode: true,
   // phoneNumber: "59162691938",
   version: [2, 3000, 1027934701],
