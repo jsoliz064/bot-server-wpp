b3139441e74307b02f2c91711cc21bca4d64eb22
jsoliz064 <jsoliz064@gmail.com>
2026-02-01 10:33:48 -0400

    updat

--- src/provider/index.js
+++ src/provider/index.js
@@ -6,5 +6,5 @@ export default createProvider(SherpaProvider, {
   // usePairingCode: true,
   // phoneNumber: "59162691938",
   version: [2, 3000, 1027934701],
-  // browser: ["Windows", "Chrome", "Chrome 114.0.5735.198"],
+  browser: ["Windows", "Chrome", "Chrome 114.0.5735.198"],
 });
